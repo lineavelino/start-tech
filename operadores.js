fa84513804ef8563edfcc4a7fff5710678934dcd
Aline Avelino <aline.savelino@gmail.com>
2021-08-09 20:52:09 -0300

    02 - Números e Operadores

---
+++ operadores.js
@@ -0,0 +1,31 @@
+//multiplicação
+var operador = 100;
+var operando = 50;
+
+var resultado = operador * operando;
+
+console.log(resultado);
+
+//concatenação string
+var nome = "Aline";
+var sobrenome = "Avelino";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+//retorno NaN
+var primeiroValor = 10;
+var segundoValor = "azul";
+
+var resultado = primeiroValor - segundoValor;
+
+console.log(resultado);
+
+//subtração - flexibilidade js de colocar número entre aspas
+var primeiroValor = 10;
+var segundoValor = '3';
+
+var resultado = primeiroValor - segundoValor;
+
+console.log(resultado);
