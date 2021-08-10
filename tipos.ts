de0da213db169023e3ba787cdca0ce7222e1d08d
Aline Avelino <aline.savelino@gmail.com>
2021-08-10 17:04:19 -0300

    05 - Múltiplos tipos com Union Types

--- tipos.ts
+++ tipos.ts
@@ -60,3 +60,12 @@ function erro(mensagem: string): never {
 function falha() { // infere que é never, pois chama uma função do tipo never
     return erro('Algo falhou');
 }
+
+// Union Types
+const nota: string | number = 5;
+function exibirNota(nota: number | string) {
+    console.log(`A nota é ${nota}`);
+}
+
+exibirNota('10');
+exibirNota(10);
