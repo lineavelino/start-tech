8d508bccd609bccbcdb607a98aa461cf689968bb
Aline Avelino <aline.savelino@gmail.com>
2021-08-14 19:49:57 -0300

    06 - Importando as rotas no componente App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { useState } from 'react';
-import axios from 'axios';
+import Routes from './routes';
 
-function App(props) {
-  const [usuario, setUsuario] = useState('');
-
-  function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
-  }
-
+function App() {
   return (
-    <>
-      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
-      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-    </>
+    <Routes />
   );
 }
 
