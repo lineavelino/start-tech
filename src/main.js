020f8e9a77e69a074342bfc110c80701e4a2f9f5
Aline Avelino <aline.savelino@gmail.com>
2021-08-16 21:56:13 -0300

    06 - Renderização, parâmetros e redirecionamentos

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createServer } from 'http';
+import { readFile } from 'fs';
+import { resolve } from 'path';
+import { parse } from 'querystring';
 
 const server = createServer((request, response) => {
     switch (request.url) {
-        case '/status': { //cria rota de status
+        case '/status': {
             response.writeHead(200, {
                 'Content-Type': 'application/json',
             });
@@ -12,6 +15,50 @@ const server = createServer((request, response) => {
                 })
             );
             response.end();
+            break;
+        }
+        case '/sign-in': {
+            const path = resolve(__dirname, './pages/sign-in.html');
+            readFile(path, (error, file) => {
+                if (error) {
+                    response.writeHead(500, "Can't process HTML file.");
+                    response.end();
+                    return;
+                }
+
+                response.writeHead(200);
+                response.write(file);
+                response.end();
+            });
+            break;
+        }
+        case '/home': {
+            const path = resolve(__dirname, './pages/home.html');
+            readFile(path, (error, file) => {
+                if (error) {
+                    response.writeHead(500, "Can't process HTML file.");
+                    response.end();
+                    return;
+                }
+
+                response.writeHead(200);
+                response.write(file);
+                response.end();
+            });
+            break;
+        }
+        case '/authenticate': {
+            let data = '';
+            request.on('data', (chunk) => {
+                data += chunk;
+            });
+            request.on('end', () => {
+                console.log(parse(data));
+                response.writeHead(301, {
+                    Location: '/home',
+                });
+                response.end();
+            });
             break;
         }
         default: {
