fd9170aadc079ee0bdb913892b72534729d1d3a9
Aline Avelino <aline.savelino@gmail.com>
2021-09-05 20:50:52 -0300

    11 - Desafio: Validador de CPF III

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 console.log('Javacript carregado');
 
 function validaCPF(cpf) {
-    return false;
+    if (cpf.length != 11) {
+        return false;
+    } else {
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring(9);
+
+        var soma = 0;
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+        console.log(soma)
+
+        return true;
+    }
 }
 
 function validacao() {
