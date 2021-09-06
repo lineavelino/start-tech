6117f0ae05bc17ab4df6da3bf273e47efaad9656
Aline Avelino <aline.savelino@gmail.com>
2021-09-05 21:32:51 -0300

    13 - Desafio: Validador de CPF V

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('Javacript carregado');
-
 function validaCPF(cpf) {
     if (cpf.length != 11) {
         return false;
@@ -11,23 +9,36 @@ function validaCPF(cpf) {
         for (var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
         }
-        console.log(soma);
 
-        var resultado = ((soma % 11) < 2) ? 0 : (11 - (soma % 11));
 
         // Validação do primeiro dígito
+        var resultado = ((soma % 11) < 2) ? 0 : (11 - (soma % 11));
         if (resultado != digitos.charAt(0)) {
             return false;
         };
 
-        console.log(digitos.toString().charAt(0) + " é a primeira posição da variável soma.")
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+
+        // Validação do segundo dígito
+        resultado = ((soma % 11) < 2) ? 0 : (11 - (soma % 11));
+
+        if (resultado != digitos.charAt(1)) {
+            return false
+        }
 
         return true;
     }
 }
 
 function validacao() {
-    console.log('Iniciando validação CPF');
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
+
     var cpf = document.getElementById('cpf-digitado').value;
 
     var resultadoValidacao = validaCPF(cpf);
