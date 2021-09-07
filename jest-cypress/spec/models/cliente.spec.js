4957d0541817985abfc3879bd1fc927174460cae
Aline Avelino <aline.savelino@gmail.com>
2021-09-07 19:08:46 -0300

    08 - Jest

--- jest-cypress/spec/models/cliente.spec.js
+++ jest-cypress/spec/models/cliente.spec.js
@@ -1,28 +1,30 @@
 const Cliente = require('../../models/cliente')
 
 describe('Clientes', () => {
-    // Ação antes do teste
     beforeEach(() => {
     })
 
-    // id, nome e telefone não podem ser undefined
-    it('Valida propriedades de um cliente', () => {
+    test('Valida propriedades de um cliente', () => {
         let cliente = Cliente.primeiro();
         expect(cliente.id).not.toBeUndefined();
         expect(cliente.nome).not.toBeUndefined();
         expect(cliente.telefone).not.toBeUndefined();
     });
 
-    // teste para retornar lista com 10 itens
-    it('Retorna todos', () => {
+    test('Retorna todos', () => {
         let clientes = Cliente.todos();
         expect(clientes.length).toEqual(10)
     });
 
-    // valida se o nome está em caixa alta
-    it('Nome do cliente Upercase', () => {
+    test('Nome do cliente Upercase', () => {
         let cliente = Cliente.primeiro();
         cliente.nome = "danilo"
         expect(cliente.nomeUpercase()).toEqual("DANILO")
+    });
+
+    test('Nome testado com indexOf', () => {
+        let cliente = Cliente.primeiro();
+        cliente.nome = "danilo"
+        expect(cliente.nome.indexOf("d") != -1).toEqual(true)
     });
 })
