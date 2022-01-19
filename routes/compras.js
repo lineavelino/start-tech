287c9afc50945f95cea2c48ec0f08131ea1feb37
Aline Avelino <aline.savelino@gmail.com>
2022-01-19 17:27:00 -0300

    Prática de integração com Node-Fetch - Parte 2

--- routes/compras.js
+++ routes/compras.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var cielo = require('../lib/cielo');
 var router = express.Router();
 
 /* POST criação de compra. */
 router.post('/', function (req, res, next) {
-  res.send('Rodando...');
+  res.send(cielo.compra(req.body));
 });
 
 /* GET status de compra. */
