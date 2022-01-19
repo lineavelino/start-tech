bb5995b6b6f025cb117ea433dbaf1f6436125d68
Aline Avelino <aline.savelino@gmail.com>
2022-01-19 18:15:07 -0300

    18 - Prática de Express - Parte 3

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const app = express()
 // Definição de porta
 const port = 3000
 
+// // Abrindo conexão com base de dados
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "sistema_noticias"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Conexão MySql ok")
+})
+
 // Serviço de Hello World
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -15,8 +28,11 @@ app.get('/', (req, res) => {
 
 // Serviço de busca de categorias
 app.get('/news-api/v1/categorias', (req, res) => {
-    let categorias = []
-    res.send(categorias)
+    // Busca categorias
+    connection.query('SELECT id, name FROM sistema_noticias.categoria', function (err, rows, fields) {
+        if (err) throw err;
+        res.send(rows)
+    });
 })
 
 // Subindo servidor Node
